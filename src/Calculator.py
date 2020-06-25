0778d7c3768ffe3657c53053f7c583e8487b3142
ap2437 <ap2437@njit.edu>
2020-06-25 00:47:56 -0400

    Feature: Setup addition

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,8 +1,14 @@
-
-
-
+def addition(a,b):
+    return a + b
+def subtraction(a,b):
+    return a - b
 class Calculator:
     result = 0
 
-    def __init__(self):
+    def _init_(self):
         pass
+
+    def add(self,a, b):
+        self.result=addition(a,b)
+        return self.result
+
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,12 +1,45 @@
 import unittest
 
 from Calculator import Calculator
-
 class MyTestCase(unittest.TestCase):
-
     def test_instantiate_calculator(self):
         calculator=Calculator()
         self.assertIsInstance(calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        calculator =Calculator()
+        self.assertEqual(calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
+        self.assertEqual(calculator.result,4)
+
+    def test_subtract_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.subtract(2, 2), 0)
+        self.assertEqual(calculator.result, 0)
+
+    def test_multiply_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.multiply(2, 2), 4)
+        self.assertEqual(calculator.result,4)
+
+    def test_divide_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.divide(2, 2), 1)
+        self.assertEqual(calculator.result, 0)
+
+    def test_square_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.square(2, 2), 4)
+        self.assertEqual(calculator.result,4)
+
+    def test_squarert_method_calculator(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.squareroot(16), 4)
+        self.assertEqual(calculator.result,4)
+
 
 
 if __name__ == '__main__':
